2105e8b5958f079b51cd84b34ed18da30f457f65
vasundra-172 <vishwakarmavasundra@gmail.com>
2024-12-04 01:32:04 +0530

    update baseURL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://homyz-agr381oal-vasundras-projects.vercel.app/api",
+  baseURL: "https://homyz-server-n1bd7oczb-vasundras-projects.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
