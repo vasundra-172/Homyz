da5e3f0c9179e3755a57f6d45af2c4d31005fcab
vasundra-172 <vishwakarmavasundra@gmail.com>
2024-12-04 01:39:11 +0530

    Auth0 updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   domain="dev-hg8zed6i4nqrkifr.us.auth0.com"
   clientId="hyXcPuHcv2OlGqE9ojTTFfm5mhxEzbMP"
   authorizationParams={{
-    redirect_uri: "https://homyz-server-n1bd7oczb-vasundras-projects.vercel.app"
+    redirect_uri: "https://homyz-agr381oal-vasundras-projects.vercel.app"
   }}
   audience="https://dev-hg8zed6i4nqrkifr.us.auth0.com/api/v2/"
   scope="openid profile email"
